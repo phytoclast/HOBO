f30711b2934ecbe26e43c6cc7bb8c01860ff8bd4
phytoclast <greg.schmidt@mi.usda.gov>
2023-11-06 16:31:24 -0500

    Create temperature model

--- processdata.R
+++ processdata.R
@@ -16,7 +16,7 @@ logdata0 <- logdata0[,c('station', 'date', 'air','depth', 't')]
 if(i==1){logdata=logdata0}else{logdata=rbind(logdata,logdata0)}
 }
 logdata <- logdata |> group_by(station,date,depth,air) |> summarise(t=mean(t))
-logdata <- loggers |> right_join(logdata) 
+logdata <- loggers |> mutate(GRR1 = ifelse(station %in% 'GRR1',1,0),GRR2 = ifelse(station %in% 'GRR2',1,0),GRR5 = ifelse(station %in% 'GRR5',1,0),GRR6 = ifelse(station %in% 'GRR6',1,0)) |> right_join(logdata) 
 
 #other stations ---- 
 enviro <- read.csv('data/envriroweather/stations.csv')
@@ -57,4 +57,101 @@ for(i in 1:length(files)){#i=29
     if(is.null(envirodata)){envirodata=e0}else{envirodata=rbind(envirodata,e0)}
   }
 }
-envirodata <- enviro |> right_join(envirodata) 
+envirodata <- enviro |> mutate(GRR1 = 0,GRR2 = 0,GRR5 = 0,GRR6 = 0) |> right_join(envirodata) 
+
+noaa.rec <- readRDS('data/noaa/NOAA_records.RDS')
+noaa.sta <- readRDS('data/noaa/NOAA_stations.RDS')
+
+noaa.sta <- noaa.sta |> mutate(station = id, lat=latitude, lon=longitude, elev=elevation) |> subset(select = c(station, lat,lon,elev))
+
+noaa.rec <- noaa.rec |> mutate(station=id, air=1, depth=0, t=(tmax+tmin)/20) |> subset(!is.na(t), select=c(station, date, air, depth, t))
+
+noaa.rec <- noaa.sta |> mutate(GRR1 = 0,GRR2 = 0,GRR5 = 0,GRR6 = 0) |> right_join(noaa.rec)
+
+alldata <- rbind(logdata, envirodata, noaa.rec) 
+
+#build models
+library(lubridate)
+
+alldata <- alldata |> mutate(decdate = decimal_date(date), 
+                             doy = (decdate - floor(decdate)), 
+                             mon = month(date),
+                             d = day(date), 
+                             sin0 = cos((doy)*2*3.141592+2.817934867), #optimal phase
+                             # sin15day = cos((doy+15/365)*2*3.141592+2.817934867), #15 day lag
+                             sin1mon = cos((doy-1/12)*2*3.141592+2.817934867), #1 month lag
+                             sinsqr = (sin0+1)^2/2
+                             # sin3mon = cos((doy-3/12)*2*3.141592+2.817934867) #3 month lag
+) |> subset(decdate >= 1960)
+
+# #test for optimal phase shift
+# sumt <- alldata |> subset(decdate > 1960 & decdate <2010 & station %in% "USW00094860") 
+# library(eegkit)
+# df2 = eegfft(sumt2$t, 1) |> mutate(p = 1/frequency)
+# totaldats <-  sumt |> group_by(mon,d)  |>  summarize(doy = mean(doy), t = mean(t), sin0=mean(sin0))
+# ggplot(totaldats, aes(x=doy, y=t))+
+#   geom_point()+
+#   geom_smooth()+
+#   geom_line(aes(x=doy, y=sin0*15+10))+
+#   geom_line(aes(x=doy, y=(sin0+1)^2/2*10+5))
+
+mod <- lm(t ~ air*sin0+depth*sin0+
+            air*sin1mon+depth*sin1mon+
+            air*sinsqr+depth*sinsqr+
+            lat+lon+elev+decdate+I(decdate^2)+
+            sin0*lat+sin0*lon+sin0*elev+
+          GRR1+GRR2+GRR5+GRR6, data=alldata)
+
+summary(mod)
+alldata <- alldata |> mutate(pred = predict(mod,alldata), res = t-pred)
+library(ranger)
+rf <- ranger(res ~ air+depth+sin0+
+         sin1mon+sinsqr+
+         lat+lon+elev+decdate+
+         GRR1+GRR2+GRR5+GRR6, data=alldata, num.trees=200, sample.fraction = 0.25)
+rf$prediction.error
+
+#test models
+alldata <- alldata |> mutate(pred2 = predictions(predict(rf, data=alldata)), pred3 = pred+pred2)
+
+newdat <- subset(alldata, station %in% 'USW00014817' & decdate >= 1961 & decdate < 1991) 
+newdat <- subset(alldata, station %in% c('GRR1','GRR2','GRR5','GRR6')) 
+newdat <- subset(alldata, station %in% 'nwmhrs' & decdate >= 2019.655 & decdate <= 2023.836) 
+
+newdat <- newdat |> group_by(station, mon, depth) |> summarize(t=mean(t), pred3=mean(pred3)) |> group_by(station, depth) |> summarize(t=mean(t), pred3=mean(pred3))
+
+
+
+#apply models to new data
+newdat.dates<- subset(alldata, select = c(date)) |> unique() |> arrange(date)
+newdat.station <- subset(alldata, #station %in% c('USW00014817', 'GRR1','GRR2','GRR5','GRR6'),
+                         select = c(station,lat,lon,elev,air,depth,GRR1,GRR2,GRR5,GRR6)) |> unique()
+
+virtual1 <- newdat.station |> mutate(depth=0,air=1)
+virtual2 <- newdat.station |> mutate(depth=10,air=0)
+virtual3 <- newdat.station |> mutate(depth=50,air=0)
+newdat.station <- rbind(virtual1,virtual2,virtual3) |> unique()
+newdat <- merge(newdat.station, newdat.dates)
+
+newdat <- newdat |> mutate(decdate = decimal_date(date), 
+                                       doy = (decdate - floor(decdate)), 
+                                       mon = month(date),
+                                       d = day(date), 
+                                       sin0 = cos((doy)*2*3.141592+2.817934867), #optimal phase
+                                       sin1mon = cos((doy-1/12)*2*3.141592+2.817934867), #1 month lag
+                                       sinsqr = (sin0+1)^2/2,
+                                      )
+
+newdat <- newdat |> mutate(pred = predict(mod,newdat), pred2 = predictions(predict(rf, data=newdat)), pred3 = pred+pred2)
+
+newdat.monthly <- newdat |> subset(decdate >= 1961 & decdate < 1991) |> group_by(station, mon, depth) |> summarise(t=mean(pred3))
+
+newdat.annual <- newdat |> subset(decdate >= 2010 & decdate < 2023) |> group_by(station, lat,lon,elev, depth) |> summarise(t=mean(pred3))
+write.csv(newdat.annual, 'annual_2010_2022.csv', row.names = F)
+
+newdat.annual <- newdat |> subset(decdate >= 1981 & decdate < 2011) |> group_by(station, lat,lon,elev, depth) |> summarise(t=mean(pred3))
+write.csv(newdat.annual, 'annual_1981_2010.csv', row.names = F)
+
+newdat.annual <- newdat |> subset(decdate >= 1961 & decdate < 1991) |> group_by(station, lat,lon,elev, depth) |> summarise(t=mean(pred3))
+write.csv(newdat.annual, 'annual_1961_1990.csv', row.names = F)
+
